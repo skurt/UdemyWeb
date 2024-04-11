f5acffc0d6f900feb597ba92f8ce01fc074c2dd3
Jan Kadera <jan.kadera@seznam.cz>
2024-04-11 14:39:24 +0200

    A commit for them Discord trolls not to look at.

---
+++ UdemyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyWeb.Data;
+using UdemyWeb.Models;
+
+namespace UdemyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name.ToLower() == category.DisplayOrder.ToString().ToLower())
+            {
+                ModelState.AddModelError("name", "Display order cannot exactly match the Category Name");
+            }
+            if (category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is a forbidden category name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            { 
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name.ToLower() == category.DisplayOrder.ToString().ToLower())
+            {
+                ModelState.AddModelError("name", "Display order cannot exactly match the Category Name");
+            }
+            if (category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is a forbidden category name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ UdemyWeb/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace UdemyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Obolebolerder")]
+        [Range(1,100, ErrorMessage = "This field must be a number between 1 and 100")]
+        public int DisplayOrder { get; set; }
+
+        public Category()
+        { 
+        }
+    }
+}
