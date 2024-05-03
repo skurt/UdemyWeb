868764cd92c8407e5717c841b5d7c1cb3da9ad82
Jan Kadera <jan.kadera@seznam.cz>
2024-05-03 08:05:23 +0200

    working version to transfer

--- UdemyWeb/Controllers/CategoryController.cs
+++ UdemyWeb/Controllers/CategoryController.cs
@@ -33,10 +33,18 @@ namespace UdemyWeb.Controllers
             {
                 ModelState.AddModelError("", "Test is a forbidden category name");
             }
+            var categories = _db.Categories.Where(x => x.DisplayOrder == category.DisplayOrder);
+            var count = categories.Count();
+            if (count > 0)
+            {
+                ModelState.AddModelError("DisplayOrder", "This number is unavailable for a Display Order, please choose another.");
+            }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category created sucessfully.";
                 return RedirectToAction("Index", "Category");
             }
             else
@@ -62,24 +70,47 @@ namespace UdemyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category category)
         {
-            if (category.Name.ToLower() == category.DisplayOrder.ToString().ToLower())
-            {
-                ModelState.AddModelError("name", "Display order cannot exactly match the Category Name");
-            }
-            if (category.Name.ToLower() == "test")
-            {
-                ModelState.AddModelError("", "Test is a forbidden category name");
-            }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(category);
+                _db.Categories.Update(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated sucessfully.";
                 return RedirectToAction("Index", "Category");
             }
             else
             {
                 return View();
             }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category " + category.Name + " deleted sucessfully!";
+
+            return RedirectToAction("Index", "Category");
         }
 
 
