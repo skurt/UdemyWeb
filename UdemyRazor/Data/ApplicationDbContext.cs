10d70a2a4e30708795f59402f0af4b33d952d998
Jan Kadera Kentico <Jan.kadera@seznam.cz>
2024-05-10 11:17:57 +0200

    Razor project added and not working

---
+++ UdemyRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyRazor.Models;
+
+namespace UdemyRazor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Category 1", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci Fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Poetry", DisplayOrder = 3 }
+                );
+        }
+    }
+}
--- UdemyRazor/Models/Category.cs
+++ UdemyRazor/Models/Category.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace UdemyWeb.Models
+namespace UdemyRazor.Models
 {
     public class Category
     {
